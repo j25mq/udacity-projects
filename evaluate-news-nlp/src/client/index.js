481b3220a2f8b7a004b292783dc6d90ed41aac8a
julie <juliemiquelard@protonmail.com>
2023-02-01 09:36:29 +0100

    Delete evaluate-news-nlp directory

--- evaluate-news-nlp/src/client/index.js
+++
@@ -1,9 +0,0 @@
-import './css/header.scss';
-import './css/form.scss';
-import './css/footer.scss';
-import './css/main.scss';
-
-import { validateURL } from './js/urlChecker';
-import { handleSubmit } from './js/formHandler';
-
-export {validateURL, handleSubmit};
