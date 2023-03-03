45f8e52041cd3d122cbc884cea180268fc6dd1b1
julie <juliemiquelard@protonmail.com>
2023-03-03 09:17:49 +0100

    Delete capstone directory

--- capstone/_test_/appTest.test.js
+++
@@ -1,7 +0,0 @@
-describe("a value entered empty should be empty", () => {
-    test("this value should be empty", () => {
-        const value = " ";
-        expect(value).toEqual(" ");
-        expect(value).not.toEqual("Toronto");
-    });
-});
--- capstone/src/client/index.js
+++
@@ -1,9 +0,0 @@
-import './styles/main.scss';
-import './styles/app.scss';
-import './styles/footer.scss';
-import './styles/header.scss';
-
-// import { validateURL } from './js/urlChecker';
-// import { handleSubmit } from './js/formHandler';
-
-// export {validateURL, handleSubmit};
