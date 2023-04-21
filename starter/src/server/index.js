5a2f5d033fbfa91b8147090d073ee8304da31414
julie <juliemiquelard@protonmail.com>
2023-04-21 09:46:50 +0200

    Add files via upload

--- starter/src/server/index.js
+++ starter/src/server/index.js
@@ -1,28 +1,62 @@
 require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+const path = require('path');
+const immutable = require('immutable');
 
-const app = express()
-const port = 3000
+// dotenv.config();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, '../public')));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// API calls
+// fetch data for Curiosity rover
+app.get("/curiosity", async (req, res) => {
+    try {
+        const curiosityData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=${process.env.API_KEY}`)
+        .then((res) => res.json())
+        res.send({curiosityData});
+    } catch (err) {
+        console.log("error", err);
+    };
+});
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+// fetch data for Opportunity rover
+app.get("/opportunity", async (req, res) => {
+    try {
+        const opportunityData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/latest_photos?api_key=${process.env.API_KEY}`)
+        .then((res) => res.json())
+        res.send({opportunityData});
+    } catch (err) {
+        console.log("error", err);
+    };
+});
 
-// your API calls
+// fetch data for Spirit rover
+app.get("/spirit", async (req, res) => {
+    try {
+        const spiritData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/latest_photos?api_key=${process.env.API_KEY}`)
+        .then((res) => res.json())
+        res.send({spiritData});
+    } catch (err) {
+        console.log("error", err);
+    };
+});
+
+// port
+const port = 3000;
+app.listen(port, () => console.log(`Mars Dashboard app listening on port ${port}.`));
 
 // example API call
 app.get('/apod', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
+        .then(res => res.json())
         res.send({ image })
     } catch (err) {
         console.log('error:', err);
     }
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+});
